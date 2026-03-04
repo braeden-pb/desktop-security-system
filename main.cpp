998f2c0f3faec4675ea45e3557e5e8112021f1d9
Evan Salmon <esalmon6@uwo.ca>
2026-03-04 15:09:48 -0500

    Add buttons to home panel

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
 //
 // Created by evan on 2026-02-19.
 //
+
+#include <wx/wx.h>
+#include "UI.h"
+#include "SecuritySystem.h"
+
+class MyApp : public wxApp {
+public:
+    bool OnInit() override {
+        SecuritySystem* system = new SecuritySystem();
+        UI* mainUi = new UI(system);
+        mainUi->Show(true);
+        mainUi->Maximize(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
