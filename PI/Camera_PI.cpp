be1a83a15f71d2d934ce29e04ca4d087b16e4171
Michael Pachowski <mpachows@uwo.ca>
2026-03-26 01:32:56 -0400

    update camera files

--- PI/Camera_PI.cpp
+++ PI/Camera_PI.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Camera_PI.h"
+#include <iostream>
+#include <stdexcept>
+
+Camera_PI::Camera_PI()
+    : resolution("1920x1080"),
+      fps(30),
+      isRecording(false),
+      devicePath("/dev/video0")
+{
+}
+
+Media Camera_PI::capturePhoto() {
+
+}
+
+void Camera_PI::startRecording() {
+
+}
+
+Media Camera_PI::stopRecording() {
+
+}
+
+void Camera_PI::setResolution(std::string resolution) {
+
+}
--- PI/Camera_PI.h
+++ PI/Camera_PI.h
@@ -5,13 +5,30 @@
 #ifndef GROUP55_CAMERA_PI_H
 #define GROUP55_CAMERA_PI_H
 
+#include <string>
+#include <chrono>
+
+struct Media {
+    std::string filePath;
+    std::string type;
+};
 
 class Camera_PI {
 public:
+    Camera_PI();
+    ~Camera_PI();
 
+    Media capturePhoto();
+    void startRecording();
+    Media stopRecording();
+    void setResolution(const std::string& res);
 
 private:
+    std::string resolution;
+    int fps;
+    bool isRecording;
+    std::chrono::system_clock::time_point lastCaptureAt;
+    std::string devicePath;
 };
 
-
-#endif //GROUP55_CAMERA_PI_H
+#endif
