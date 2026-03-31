c13b2e35618203b3597bb86462096db0b9258868
Evan Salmon <esalmon6@uwo.ca>
2026-03-31 00:09:21 -0400

    fix looping and path issue

--- PI/Alarm_PI.cpp
+++ PI/Alarm_PI.cpp
@@ -34,7 +34,7 @@ Alarm_PI::~Alarm_PI() {
 void Alarm_PI::soundAlarm(std::string soundOption) {
     sound = soundOption;
     alarmPlaying = true;
-    std::string cmd = "while true; do aplay -D hw:2,0 /PI/sounds/" + sound + ".wav; done &";
+    std::string cmd = "aplay -D hw:2,0 --loops=0 ../sounds/" + sound + ".wav &";
     int result = system(cmd.c_str());
     if (result != 0) {
         std::cerr << "Failed to start alarm, error: " << result << std::endl;
