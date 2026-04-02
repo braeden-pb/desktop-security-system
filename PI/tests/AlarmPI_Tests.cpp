7bd6105356c3c747853a5c5686e72f19e0eddeae
braed <bpatiern@uwo.ca>
2026-04-02 14:08:10 -0400

    Added PI Test cases

--- PI/tests/AlarmPI_Tests.cpp
+++ PI/tests/AlarmPI_Tests.cpp
@@ -6,7 +6,7 @@
 #include "../Alarm_PI.h"
 #include "../NetworkServer.h"
 
-class AlarmPITests : public ::testing::Test {
+class AlarmPI_Tests : public ::testing::Test {
 protected:
     void SetUp() override {
         server = new NetworkServer(0); // port 0 = any available port
@@ -25,14 +25,14 @@ protected:
 /**
  * @test Alarm should not be playing on construction.
  */
-TEST_F(AlarmPITests, InitiallyNotPlaying) {
+TEST_F(AlarmPI_Tests, InitiallyNotPlaying) {
     EXPECT_FALSE(alarm->isPlaying());
 }
 
 /**
  * @test soundAlarm() should set alarm to playing state.
  */
-TEST_F(AlarmPITests, SoundAlarmSetsPlayingTrue) {
+TEST_F(AlarmPI_Tests, SoundAlarmSetsPlayingTrue) {
     alarm->soundAlarm("alarm");
     EXPECT_TRUE(alarm->isPlaying());
     alarm->disableAlarm();
@@ -41,7 +41,7 @@ TEST_F(AlarmPITests, SoundAlarmSetsPlayingTrue) {
 /**
  * @test disableAlarm() should stop the alarm.
  */
-TEST_F(AlarmPITests, DisableAlarmSetsPlayingFalse) {
+TEST_F(AlarmPI_Tests, DisableAlarmSetsPlayingFalse) {
     alarm->soundAlarm("alarm");
     alarm->disableAlarm();
     EXPECT_FALSE(alarm->isPlaying());
@@ -50,14 +50,14 @@ TEST_F(AlarmPITests, DisableAlarmSetsPlayingFalse) {
 /**
  * @test disableAlarm() when not playing should not crash.
  */
-TEST_F(AlarmPITests, DisableWhenNotPlayingIsNoOp) {
+TEST_F(AlarmPI_Tests, DisableWhenNotPlayingIsNoOp) {
     EXPECT_NO_THROW(alarm->disableAlarm());
 }
 
 /**
  * @test soundAlarm() called twice should not crash.
  */
-TEST_F(AlarmPITests, SoundAlarmTwiceDoesNotCrash) {
+TEST_F(AlarmPI_Tests, SoundAlarmTwiceDoesNotCrash) {
     EXPECT_NO_THROW({
         alarm->soundAlarm("alarm");
         alarm->soundAlarm("alarm");
