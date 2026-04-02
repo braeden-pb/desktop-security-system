4aa7a4a004839139dcd321a6c8c0fc0ba1b3b3d1
Evan Salmon <esalmon6@uwo.ca>
2026-04-02 15:16:38 -0400

    fix header issue

--- src/SecuritySystem.cpp
+++ src/SecuritySystem.cpp
@@ -6,6 +6,15 @@
  */
 
 #include "SecuritySystem.h"
+#include "Alert.h"
+#include "Alarm.h"
+#include "Config.h"
+#include "UI.h"
+#include "Storage.h"
+#include "Alert.h"
+#include "Camera.h"
+#include "Motion_Sensor.h"
+#include "Network.h"
 
 /**
  * @brief Constructs a fully initialized SecuritySystem with UI, storage, and alarm.
