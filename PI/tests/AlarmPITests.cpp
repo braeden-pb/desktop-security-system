f888f15b55db48cff3217a5d635d130bddf0ee67
braed <bpatiern@uwo.ca>
2026-04-02 14:00:29 -0400

    Added PI Test cases

---
+++ PI/tests/AlarmPITests.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Braeden Patierno-Barker on 3/31/2026.
+//
+
+#include <gtest/gtest.h>
+
---
+++ PI/tests/CameraPITests.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Braeden Patierno-Barker on 3/31/2026.
+//
+
+#include <gtest/gtest.h>
